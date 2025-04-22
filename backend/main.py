c769ea8b1d0d728e0bd52d8a0289ad289b054f1a
StuartUni <stuartsmith87uni@gmail.com>
2025-04-22 04:48:37 +0100

    updates allow_origins

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 #  Enable CORS (Adjust frontend URLs in future config.py)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://machine-learning-enhanced-pc-component-iyr1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
